65edfa7325e5967f75a7a57352c32ed1d6320e5d
CodeMancer <86000407+NaveenShaji742@users.noreply.github.com>
2025-06-10 18:57:54 +0400

    Delete backend/src/main/java/com/Project/project/repository/MilestonePhaseRepository.java

--- backend/src/main/java/com/Project/project/repository/MilestonePhaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.project.repository;
-
-import com.Project.project.model.MilestonePhase;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface MilestonePhaseRepository extends JpaRepository<MilestonePhase, Long> {
-    Optional<MilestonePhase> findByName(String name);
-}
