ffdb599e41bbf3cf50d3109b0adc277f06fad7aa
CodeMancer <86000407+NaveenShaji742@users.noreply.github.com>
2025-06-10 18:58:20 +0400

    Delete backend/src/main/java/com/Project/project/exception/MilestonePhaseNotFoundException.java

--- backend/src/main/java/com/Project/project/exception/MilestonePhaseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.Project.project.exception;
-
-public class MilestonePhaseNotFoundException extends RuntimeException {
-    public MilestonePhaseNotFoundException(Long id) {
-        super("Milestone Phase with ID " + id + " not found.");
-    }
-}
